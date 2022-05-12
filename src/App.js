f539a082b3114644d5fce993c0737bfd66e88a8a
jpngeekudin <udinshalah12@gmail.com>
2022-05-12 23:15:23 +0700

    FE update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,114 @@
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'piramore';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
+const TEST_GIFS = [
+  'https://media4.giphy.com/media/ensLRi1D2wRXMR4Gnr/giphy.gif?cid=ecf05e47tgxhdeybmzeh1613ba8dje0b87bs6tsj75jhso46&rid=giphy.gif&ct=g'
+];
+
 const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
+  const [inputValue, setInputValue] = useState('');
+  const [gifList, setGifList] = useState([]);
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+      if (solana) {
+        if (solana.isPhantom) console.log('Phantom wallet found!');
+        const response = await solana.connect({ onlyIfTrusted: true });
+        console.log('Connected with public key: ', response.publicKey.toString());
+        setWalletAddress(response.publicKey.toString());
+      }
+      
+      else {
+        alert('Solana object not found! Go get phantom wallet ehhe.');
+      }
+    }
+    
+    catch(err) {
+      console.log(err);
+    }
+  }
+
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log('Connected with public key: ', response.publicKey.toString());
+      setWalletAddress(solana.publicKey.toString());
+    }
+  }
+
+  const sendGif = async () => {
+    if (inputValue.length > 0) {
+      console.log('GIF link: ', inputValue);
+      setGifList([...gifList, inputValue]);
+      setInputValue('');
+    }
+    else console.log('Empty input, try again.');
+  }
+
+  const onInputChange = (event) => {
+    const { value } = event.target;
+    setInputValue(value);
+  }
+
+  const renderNotConnectedContainer = () => (
+    <button className='cta-button connect-wallet-button' onClick={connectWallet}>
+      Connect to Wallet
+    </button>
+  );
+
+  const renderConnectedContainer = () => (
+    <div className="connected-container">
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        sendGif();
+      }}>
+        <input type="text" placeholder="Enter gif link!" value={inputValue} onChange={onInputChange} />
+        <button type="submit" className="cta-button submit-gif-button">Submit</button>
+      </form>
+      <div className="gif-grid">
+        {gifList.map(gif => (
+          <div className="gif-item" key={gif}>
+            <img src={gif} alt={gif} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    }
+
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
+  useEffect(() => {
+    if (walletAddress) {
+      setGifList(TEST_GIFS);
+    }
+  }, [walletAddress]);
+
   return (
     <div className="App">
-      <div className="container">
+      <div className={walletAddress ? 'authed-container' : 'container'}>
         <div className="header-container">
           <p className="header">🖼 GIF Portal</p>
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
+          {!walletAddress && renderNotConnectedContainer()}
+          {walletAddress && renderConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
